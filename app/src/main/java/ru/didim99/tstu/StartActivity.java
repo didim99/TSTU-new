89840c8956dc78415fcfb463b6a79104d4287218
didim99 <didim@eclabs.ru>
2018-09-06 19:15:26 +0300

    Initial commit

---
+++ app/src/main/java/ru/didim99/tstu/StartActivity.java
@@ -0,0 +1,15 @@
+package ru.didim99.tstu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class StartActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.act_start);
+
+  }
+}
---
+++ app/src/main/java/ru/didim99/tstu/core/translator/LexicalAnalyzer.java
@@ -0,0 +1,7 @@
+package ru.didim99.tstu.core.translator;
+
+/**
+ * Created by didim99 on 06.09.18.
+ */
+public class LexicalAnalyzer {
+}
