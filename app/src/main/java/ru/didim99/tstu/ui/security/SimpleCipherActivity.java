326bd84302aa43b405b770b51a007b44ebce9778
didim99 <didim@eclabs.ru>
2020-09-09 12:05:08 +0300

    + [is] Added UI stub

--- app/src/main/java/ru/didim99/tstu/ui/security/SimpleCipherActivity.java
+++ app/src/main/java/ru/didim99/tstu/ui/security/SimpleCipherActivity.java
@@ -1,5 +1,7 @@
 package ru.didim99.tstu.ui.security;
 
+import android.os.Bundle;
+import ru.didim99.tstu.R;
 import ru.didim99.tstu.ui.BaseActivity;
 import ru.didim99.tstu.utils.MyLog;
 
@@ -10,4 +12,16 @@ import ru.didim99.tstu.utils.MyLog;
 public class SimpleCipherActivity extends BaseActivity {
   private static final String LOG_TAG = MyLog.LOG_TAG_BASE + "_SCAct";
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    MyLog.d(LOG_TAG, "SimpleCipherActivity creating...");
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.act_simple_cipher);
+
+    MyLog.d(LOG_TAG, "View components init...");
+
+    MyLog.d(LOG_TAG, "View components init completed");
+
+    MyLog.d(LOG_TAG, "SimpleCipherActivity created");
+  }
 }
